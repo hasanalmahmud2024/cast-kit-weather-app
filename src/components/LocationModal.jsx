b504c6945dbecc46a4935bfdad121817419391e9
noiraxen <noiraxen@proton.me>
2026-09-24 10:21:23 +0600

    get browser Geolocation API to retrieve the user's latitude and longitude

--- src/components/LocationModal.jsx
+++ src/components/LocationModal.jsx
@@ -1,4 +1,4 @@
-import { X } from "lucide-react";
+import { LocateFixed, X } from "lucide-react";
 import { useState } from "react";
 
 const LocationModal = ({ onClose }) => {
@@ -9,6 +9,22 @@ const LocationModal = ({ onClose }) => {
         const value = city.trim()
         console.log(value)
     }
+
+    /**
+    Use the browser Geolocation API to retrieve the user's current latitude and longitude when they select "Use My Location", with a 10-second timeout and basic error handling for denied or unavailable location access.
+     */
+    const handleGeolocation = () => {
+        navigator.geolocation.getCurrentPosition((positions) => {
+            const { latitude, longitude } = positions.coords;
+            console.log(latitude, longitude)
+
+        }, (error) => {
+            console.log(error)
+        }, {
+            timeout: 10000
+        })
+    }
+
     return (
         <div className="fixed inset-0 flex items-center justify-center bg-gray-900/60">
             <div className="h-80 w-96 bg-gray-100 shadow-2xl rounded-3xl p-5">
@@ -27,7 +43,7 @@ const LocationModal = ({ onClose }) => {
                             placeholder="Enter Your City"
                             className="px-3 py-1.5 border rounded-4xl w-full"
                             value={city}
-                            onChange={(e)=>setCity(e.target.value)}
+                            onChange={(e) => setCity(e.target.value)}
                         />
                         <div>
                             <button
@@ -39,15 +55,18 @@ const LocationModal = ({ onClose }) => {
                         </div>
                     </form>
                 </div>
-                
+
                 <div className="text-center py-2">Or</div>
 
                 <div>
                     <button
                         type="button"
-                        className="rounded-4xl w-full bg-blue-500 px-5 py-2 text-md font-medium text-white transition-all delay-100 hover:scale-110"
+                        onClick={handleGeolocation}
+                        className="rounded-4xl w-full text-blue-500 border-2 border-blue-500 px-5 py-2 text-md font-medium bg-white transition-all delay-100 hover:scale-110"
                     >
-                        Use My Location
+                        <div className="flex justify-center gap-3">
+                            <LocateFixed /> <span>Use My Location</span>
+                        </div>
                     </button>
                 </div>
             </div>
